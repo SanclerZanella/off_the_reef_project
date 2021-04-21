b813a7872d76b657706ef38c253c7224e689e92e
Sancler Zanella <sanclerzjj@gmail.com>
2021-04-21 23:00:13 +0000

    Remove the songs suggestion section at the bottom of the home page, to improve the UX; ADD style to the first part of report section at the top of index.html

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -105,66 +105,7 @@ $(document).ready(function () {
             };
         });
     });
-
-    // -----------------------------------------------------------------------------Songs suggestion slide Script
-    // fetch all elements with slideContent class
-    let slideContent = $('.slideContent');
-
-    // Add functionality on click to the previous button
-    $('#leftArrow').click(function () {
-
-        // fetch the current element with activeSlide class, which is shown on screen while the others slide contents are hidden
-        let currentDailyPrevSlide = $('.activeSlide');
-
-        // fetch the previous element before the element with activeSlide class, 
-        //which is hidden on screen
-        let prevSlide = currentDailyPrevSlide.prev();
-
-        // if there is a sibling element before the current element with activeSlide class
-        if (prevSlide.length) {
-
-            // then will remove the activeSlide class of the current element, getting this element hidden on flow
-            currentDailyPrevSlide.removeClass('activeSlide');
-
-            // and will add activeSlide class to the previous sibling element, getting this element visible on flow
-            prevSlide.addClass('activeSlide');
-
-            // if there is not a sibling element before the current element with activeSlide class
-        } else if (!prevSlide.length) {
-
-            // then will remove the activeSlide class of the current element, getting this element hidden on flow
-            currentDailyPrevSlide.removeClass('activeSlide');
-
-            // and will add activeSlide class to the last sibling element, getting this element visible on flow
-            slideContent.last().addClass('activeSlide');
-        }
-    });
-
-    // Add functionality on click to the next arrow button
-
-    $('#rigthArrow').click(function () {
-        let currentDailyPrevSlide = $('.activeSlide');
-
-        // fetch the next element after the element with activeSlide class, 
-        //which is hidden on screen
-        let nextSlide = currentDailyPrevSlide.next();
-
-        if (nextSlide.length) {
-            currentDailyPrevSlide.removeClass('activeSlide');
-
-            // fetch the next element after the element with activeSlide class, 
-            //which is hidden on screen
-            nextSlide.addClass('activeSlide');
-
-            // if there is not a sibling element after the current element with activeSlide class
-        } else if (!nextSlide.length) {
-            currentDailyPrevSlide.removeClass('activeSlide');
-
-            // will add activeSlide class to the first sibling element, getting this element visible on flow
-            slideContent.first().addClass('activeSlide');
-        }
-    });
-})
+});
 
 
 // -----------------------------------------------------------------------------Current Time Script
@@ -172,10 +113,12 @@ $(document).ready(function () {
 function currentHour() {
 
     //Fetch the current time
-    let currentTime = new Date().toString().substr(0, 24);
+    let currentTime = new Date();
+    let date = currentTime.toString().substr(0, 15);
+    let time = currentTime.toString().substr(15, 10);
 
     //Apply the current time to an element in the document
-    $('#currentTime').html(currentTime);
+    $('#currentTime').html(`<p>${date} - ${time} </p>`);
 
     //Refresh the time each 1 second, set in milli seconds
     setTimeout('currentHour()', 1000)
