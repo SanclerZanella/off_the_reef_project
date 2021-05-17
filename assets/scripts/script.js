e9d6323664770e1de666a0acee22eee735ea2e85
Sancler Zanella <sanclerzjj@gmail.com>
2021-05-17 20:07:47 +0000

    Add functionality to get the report navbar and the reports itself to work, showing and hiding the different reports when clicking the navbar item (now, hourly, daily and surf forecast) and the reports hours and days as well

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,205 +2,58 @@
 $(document).ready(function () {
 
     // -----------------------------------------------------------------------------Hourly and daily report slide Script
-    let hourlySlideContent = $('.hourlyReportSlide');
-    let hourlySmScreen = $('.smHourlyReportSlide');
-    let dailySlideContent = $('.dailyReportSlide');
-    let dailySmScreen = $('.dailySmReportSlide');
-    let surfSlideContent = $('.SurfHourlyReportSlide');
-    let surfSmScreen = $('.smSurfHourlyReportSlide');
 
-    // Report Carousel Control
-    $('.arrowsControl').each(function () {
-        $(this).click(function () {
-            let arrowSide = $(this).attr('id');
+    $('.forecast-item').each((key, value) => {
+        $(value).click(function () {
+            let navOption = $(value).text();
 
-            switch (arrowSide) {
-
-                // Hourly weather forecast Slide script
-                case "hourlyPrev":
-                    let currentHourlyPrevSlide = $('.activeHourlyReportSlide').attr('class', 'col-lg-12 col-md-10 hourlyReportSlide activeHourlyReportSlide');
-                    let prevSlide = currentHourlyPrevSlide.prev();
-
-                    if (prevSlide.length) {
-                        currentHourlyPrevSlide.removeClass('activeHourlyReportSlide');
-                        prevSlide.addClass('activeHourlyReportSlide');
-
-                    } else if (!prevSlide.length) {
-                        currentHourlyPrevSlide.removeClass('activeHourlyReportSlide');
-                        hourlySlideContent.last().addClass('activeHourlyReportSlide');
-
-                    };
+            switch (navOption) {
+                case "Now":
+                    $('.forecast-item').removeClass('forecastActive');
+                    $(value).addClass('forecastActive');
+                    $('.reportSection').hide(500);
+                    $('.currentWeather').toggle(500);
                     break;
-
-                case "hourlyNext":
-                    let currentHourlyNextSlide = $('.activeHourlyReportSlide').attr('class', 'col-lg-12 col-md-10 hourlyReportSlide activeHourlyReportSlide');
-                    let nextSlide = currentHourlyNextSlide.next();
-
-                    if (nextSlide.length) {
-                        currentHourlyNextSlide.removeClass('activeHourlyReportSlide');
-                        nextSlide.addClass('activeHourlyReportSlide');
-
-                    } else if (!nextSlide.length) {
-                        currentHourlyNextSlide.removeClass('activeHourlyReportSlide');
-                        hourlySlideContent.first().addClass('activeHourlyReportSlide');
-
-                    };
+                case "Hourly":
+                    $('.forecast-item').removeClass('forecastActive');
+                    $(value).addClass('forecastActive');
+                    $('.reportSection').hide(500);
+                    $('.hourlyWeather').toggle(500);
                     break;
-
-                    // Hourly weather forecast Slide script for medium and small screens
-                case "smHourlyPrev":
-                    let smHourlyPrevSlide = $('.activeSmHourlyReportSlide').attr('class', 'col-lg-12 col-md-12 smHourlyReportSlide activeSmHourlyReportSlide');
-                    let smPrevSlide = smHourlyPrevSlide.prev();
-
-                    if (smPrevSlide.length) {
-                        smHourlyPrevSlide.removeClass('activeSmHourlyReportSlide');
-                        smPrevSlide.addClass('activeSmHourlyReportSlide');
-
-                    } else if (!smPrevSlide.length) {
-                        smHourlyPrevSlide.removeClass('activeSmHourlyReportSlide');
-                        hourlySmScreen.last().addClass('activeSmHourlyReportSlide');
-
-                    };
+                case "Daily":
+                    $('.forecast-item').removeClass('forecastActive');
+                    $(value).addClass('forecastActive');
+                    $('.reportSection').hide(500);
+                    $('.dailyWeather').toggle(500);
                     break;
-
-                case "smHourlyNext":
-                    let smHourlyNextSlide = $('.activeSmHourlyReportSlide').attr('class', 'col-lg-12 col-md-12 smHourlyReportSlide activeSmHourlyReportSlide');
-                    let smNextSlide = smHourlyNextSlide.next();
-
-                    if (smNextSlide.length) {
-                        smHourlyNextSlide.removeClass('activeSmHourlyReportSlide');
-                        smNextSlide.addClass('activeSmHourlyReportSlide');
-
-                    } else if (!smNextSlide.length) {
-                        smHourlyNextSlide.removeClass('activeSmHourlyReportSlide');
-                        hourlySmScreen.first().addClass('activeSmHourlyReportSlide');
-
-
-                    };
-                    break;
-
-                    // Daily weather forecast Slide script
-                case "dailyPrev":
-                    let currentDailyPrevSlide = $('.activeDailyReportSlide').attr('class', 'col-lg-12 dailyReportSlide activeDailyReportSlide');
-                    let dailyPrevSlide = currentDailyPrevSlide.prev();
-
-                    if (dailyPrevSlide.length) {
-                        currentDailyPrevSlide.removeClass('activeDailyReportSlide');
-                        dailyPrevSlide.addClass('activeDailyReportSlide');
-
-                    } else if (!dailyPrevSlide.length) {
-                        currentDailyPrevSlide.removeClass('activeDailyReportSlide');
-                        dailySlideContent.last().addClass('activeDailyReportSlide');
-
-                    };
-                    break;
-
-                case "dailyNext":
-                    let currentDailyNextSlide = $('.activeDailyReportSlide').attr('class', 'col-lg-12 dailyReportSlide activeDailyReportSlide');
-                    let dailyNextSlide = currentDailyNextSlide.next();
-
-                    if (dailyNextSlide.length) {
-                        currentDailyNextSlide.removeClass('activeDailyReportSlide');
-                        dailyNextSlide.addClass('activeDailyReportSlide');
-
-                    } else if (!dailyNextSlide.length) {
-                        currentDailyNextSlide.removeClass('activeDailyReportSlide');
-                        dailySlideContent.first().addClass('activeDailyReportSlide');
-                    };
-                    break;
-
-                    // Daily weather forecast Slide script for medium and small screens
-                case "smDailyPrev":
-                    let currentSmDailyPrevSlide = $('.activeSmDailyReportSlide').attr('class', 'col-lg-12 dailySmReportSlide activeSmDailyReportSlide');
-                    let smDailyPrevSlide = currentSmDailyPrevSlide.prev();
-
-                    if (smDailyPrevSlide.length) {
-                        currentSmDailyPrevSlide.removeClass('activeSmDailyReportSlide');
-                        smDailyPrevSlide.addClass('activeSmDailyReportSlide');
-
-                    } else if (!smDailyPrevSlide.length) {
-                        currentSmDailyPrevSlide.removeClass('activeSmDailyReportSlide');
-                        dailySmScreen.last().addClass('activeSmDailyReportSlide');
-
-                    };
-                    break;
-
-                case "smDailyNext":
-                    let currentSmDailyNextSlide = $('.activeSmDailyReportSlide').attr('class', 'col-lg-12 dailySmReportSlide activeSmDailyReportSlide');
-                    let smDailyNextSlide = currentSmDailyNextSlide.next();
-
-                    if (smDailyNextSlide.length) {
-                        currentSmDailyNextSlide.removeClass('activeSmDailyReportSlide');
-                        smDailyNextSlide.addClass('activeSmDailyReportSlide');
-
-                    } else if (!smDailyNextSlide.length) {
-                        currentSmDailyNextSlide.removeClass('activeSmDailyReportSlide');
-                        dailySmScreen.first().addClass('activeSmDailyReportSlide');
-                    };
-                    break;
-
-                    // Hourly surf forecast Slide script
-                case "surfHourlyPrev":
-                    let currentSurfPrevSlide = $('.activeSurfHourlyReportSlide').attr('class', 'col-lg-12 SurfHourlyReportSlide activeSurfHourlyReportSlide');
-                    let surfPrevSlide = currentSurfPrevSlide.prev();
-
-                    if (surfPrevSlide.length) {
-                        currentSurfPrevSlide.removeClass('activeSurfHourlyReportSlide');
-                        surfPrevSlide.addClass('activeSurfHourlyReportSlide');
-
-                    } else if (!surfPrevSlide.length) {
-                        currentSurfPrevSlide.removeClass('activeSurfHourlyReportSlide');
-                        surfSlideContent.last().addClass('activeSurfHourlyReportSlide');
-                    };
-                    break;
-
-                case "surfHourlyNext":
-                    let currentSurfNextSlide = $('.activeSurfHourlyReportSlide').attr('class', 'col-lg-12 SurfHourlyReportSlide activeSurfHourlyReportSlide');
-                    let surfNextSlide = currentSurfNextSlide.next();
-
-                    if (surfNextSlide.length) {
-                        currentSurfNextSlide.removeClass('activeSurfHourlyReportSlide');
-                        surfNextSlide.addClass('activeSurfHourlyReportSlide');
-
-                    } else if (!surfNextSlide.length) {
-                        currentSurfNextSlide.removeClass('activeSurfHourlyReportSlide');
-                        surfSlideContent.first().addClass('activeSurfHourlyReportSlide');
-                    };
-                    break;
-
-                    // Hourly surf forecast Slide script for medium and small screens
-                case "smSurfHourlyPrev":
-                    let currentSmSurfPrevSlide = $('.activeSmSurfHourlyReportSlide').attr('class', 'col-lg-12 smSurfHourlyReportSlide activeSmSurfHourlyReportSlide');
-                    let smSurfPrevSlide = currentSmSurfPrevSlide.prev();
-
-                    if (smSurfPrevSlide.length) {
-                        currentSmSurfPrevSlide.removeClass('activeSmSurfHourlyReportSlide');
-                        smSurfPrevSlide.addClass('activeSmSurfHourlyReportSlide');
-
-                    } else if (!smSurfPrevSlide.length) {
-                        currentSmSurfPrevSlide.removeClass('activeSmSurfHourlyReportSlide');
-                        surfSmScreen.last().addClass('activeSmSurfHourlyReportSlide');
-                    };
-                    break;
-
-                case "smSurfHourlyNext":
-                    let currentSmSurfNextSlide = $('.activeSmSurfHourlyReportSlide').attr('class', 'col-lg-12 smSurfHourlyReportSlide activeSmSurfHourlyReportSlide');
-                    let smSurfNextSlide = currentSmSurfNextSlide.next();
-
-                    if (smSurfNextSlide.length) {
-                        currentSmSurfNextSlide.removeClass('activeSmSurfHourlyReportSlide');
-                        smSurfNextSlide.addClass('activeSmSurfHourlyReportSlide');
-
-                    } else if (!smSurfNextSlide.length) {
-                        currentSmSurfNextSlide.removeClass('activeSmSurfHourlyReportSlide');
-                        surfSmScreen.first().addClass('activeSmSurfHourlyReportSlide');
-                    };
+                case "Surf Forecast":
+                    $('.forecast-item').removeClass('forecastActive');
+                    $(value).addClass('forecastActive');
+                    $('.reportSection').hide(500);
+                    $('.surfForecast').toggle(500);
                     break;
             };
         });
     });
 });
 
+$('.reportSection').each((key, value) => {
+
+    $(value).click(() => {
+        if ($(value).find('.arrowReport').children().attr('class') === 'fas fa-caret-down') {
+            $('.arrowReport').children().removeClass('fa-caret-up');
+            $('.arrowReport').children().addClass('fa-caret-down');
+            $('.valueTable').hide(500);
+            $(value).find('.valueTable').show(500);
+            $(value).find('.arrowReport').children().removeClass('fa-caret-down');
+            $(value).find('.arrowReport').children().addClass('fa-caret-up');
+        } else {
+            $(value).find('.arrowReport').children().removeClass('fa-caret-up');
+            $(value).find('.arrowReport').children().addClass('fa-caret-down');
+            $(value).find('.valueTable').hide(500);
+        }
+    });
+});
 
 //Current Time Script
 function currentHour() {
@@ -239,6 +92,10 @@ $(window).scroll(() => {
 // Scroll up to the map from the report section and hide the report when click the scroll top button
 scrollButton.click(() => {
     $(window).scrollTop(200);
+    $('#beachReport').find('.forecast-item').removeClass('forecastActive');
+    $('#beachReport').find('.forecast-item').first().addClass('forecastActive');
+    $('.reportSection').hide();
+    $('.reportSection').first().show();
     $('#beachReport').hide();
 });
 
